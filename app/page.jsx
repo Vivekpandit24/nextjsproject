23c12d0bed3332511667ba252f7b982e51186434
Vivekpandit24 <vivekpandit242424@gmail.com.com>
2024-11-15 14:42:11 +0530

    Final commit

---
+++ app/page.jsx
@@ -0,0 +1,143 @@
+"use client"
+
+import "./index.css";
+import Canvas from "./Canvas";
+import data from "./data";
+import LocomotiveScroll from "locomotive-scroll";
+import { useEffect, useState, useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { Circ, Expo } from "gsap/all";
+
+function Page() {
+  const [showCanvas, setShowCanvas] = useState(false);
+  const headingref = useRef(null);
+  const growingSpan = useRef(null);
+
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll();
+  }, []);
+
+  useEffect(() => {
+    const handleClick = (e) => {
+      setShowCanvas((prevShowCanvas) => {
+        if (!prevShowCanvas) {
+          gsap.set(growingSpan.current, {
+            top: e.clientY,
+            left: e.clientX,
+          });
+
+          gsap.to("body", {
+            color: "#000",
+            backgroundColor: "#fd2c2a",
+            duration: 1.2,
+            ease: "power2.inOut",
+          });
+
+          gsap.to(growingSpan.current, {
+            scale: 1000,
+            duration: 2,
+            ease: "power2.inOut",
+            onComplete: () => {
+              gsap.set(growingSpan.current, {
+                scale: 0,
+                clearProps: "all",
+              });
+            },
+          });
+        } else {
+          gsap.to("body", {
+            color: "#fff",
+            backgroundColor: "#000",
+            duration: 1.2,
+            ease: "power2.inOut",
+          });
+        }
+
+        return !prevShowCanvas;
+      });
+    };
+
+    const headingElement = headingref.current;
+    headingElement.addEventListener("click", handleClick);
+
+    // Clean up event listener on unmount
+    return () => headingElement.removeEventListener("click", handleClick);
+  }, []);
+
+  return (
+    <>
+      <span
+        ref={growingSpan}
+        className="growing rounded-full block fixed top-[-20px] left-[-20px] w-5 h-5"
+      ></span>
+      <div className="w-full relative min-h-screen font-['Helvetica_Now_Display']">
+        {showCanvas &&
+          data[0].map((canvasdets, index) => <Canvas details={canvasdets} />)}
+        <div className="w-full relative z-[1] h-screen ">
+          <nav className="w-full p-8 flex justify-between z-50">
+            <div className="brand text-2xl font-md">Vivek Pandit</div>
+            <div className="links flex gap-10">
+              {[
+                "vivekpandit242424@gmail.com",
+                " India",
+                "Job Ready",
+                "Email To hire",
+              ].map((link, index) => (
+                <a
+                  key={index}
+                  href={`#${link.toLowerCase()}`}
+                  className="text-md hover:text-gray-300"
+                >
+                  {link}
+                </a>
+              ))}
+            </div>
+          </nav>
+          <div className="textcontainer  w-full px-[20%]">
+            <div className="text w-[50%]">
+              <h3 className="text-3xl leading-[1.2]">
+              Expertise in building scalable applications with user-friendly 
+              interfaces, optimized for performance and user engagement.
+              </h3>
+              <p className="text-lg w-[80%] mt-10 font-normal">
+              Strong analytical skills to resolve complex 
+              technical challenges, ensuring high-quality performance and stability. 
+              
+ 
+              </p>
+              <p className="text-md mt-10">Click on my NAME below. ↓(This website is not responsive .  Desktop ONLY.)</p>
+            </div>
+          </div>
+          <div className="w-full absolute bottom-0 left-0">
+            <h1
+              ref={headingref}
+              className="text-[17rem] font-normal tracking-tight leading-none pl-5"
+            >
+              Vivek Pandit
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="w-full relative h-screen  mt-32 px-10">
+        {showCanvas &&
+          data[1].map((canvasdets, index) => <Canvas details={canvasdets} />)}
+        <h1 className="text-8xl tracking-tighter">About Me</h1>
+        <div className="flex"> 
+        <p className="text-4xl leading-[1.8] w-[80%] mt-10 font-light">
+        Highly skilled Full Stack Developer with 3+ years of experience designing, developing, and 
+scaling web and mobile applications across diverse sectors (healthcare, e-commerce, 
+non-profit). Expertise in MERN stack (MongoDB, Express.js, React, Node.js) and advanced 
+tools (Next.js, GSAP, JWT). Proven ability to translate client needs into user-centric, 
+efficient solutions. Seeking a challenging role to leverage technical expertise and 
+collaborative skills within a progressive development team.
+        </p>
+
+       
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Page;
